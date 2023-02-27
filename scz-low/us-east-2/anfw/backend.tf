eb1261c694dc1dfd07712b3e4089b7426cfe2327
mw-gh-jcastano <john.castano@mavenwave.com>
2023-02-27 15:24:59 -0800

    corrected region in anfw backend

--- scz-low/us-east-2/anfw/backend.tf
+++ scz-low/us-east-2/anfw/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "s3-anfw-scz-low-us-west-2"
-    key     = "aws-anfw-module-scz-low_us-west-2_anfw.tfstate"
-    region  = "us-west-2"
+    bucket  = "s3-anfw-scz-low-us-east-2"
+    key     = "aws-anfw-module-scz-low_us-east-2_anfw.tfstate"
+    region  = "us-east-2"
     profile = "mw-occ"
   }
 }
