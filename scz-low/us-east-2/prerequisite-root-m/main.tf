3847afcb27e90859588f432f9755a38e67643863
mw-gh-jcastano <john.castano@mavenwave.com>
2023-02-28 13:40:34 -0800

    terraform fmt and remove unneeded code blocks

--- scz-low/us-east-2/prerequisite-root-m/main.tf
+++ scz-low/us-east-2/prerequisite-root-m/main.tf
@@ -2,34 +2,14 @@ module "prerequisites" {
   source = "git::ssh://github.com:/mw-gh-jcastano/aws-anfw-module.git//modules/prerequisites?ref=castano-prerequisite-source-module"
   #  version              = "> 0"
 
-#  cloud_logs_enabled   = true
-
-  #  account_name         = data.aws_caller_identity.current.account.id
-#  account_name         = "John-Castano"
+  #  cloud_logs_enabled   = true
   #  flow_iam_role_arn    = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/flow-logs-role"
-  #  region               = data.aws_region_current.name
 
-
-  security_zone        = "scz_low"
-  region               = "us-east-2"
-
+  security_zone = "scz_low"
+  region        = "us-east-2"
   super_cidr_block = "10.101.0.0/16"
-//The 1st /20 in 10.10x.0.0/16
-#  edge_vpc_cidr = cidrsubnet(local.super_cidr_block, 4, 0)
-  // The 2nd /20 in 10.10x.0.0/16
-#  shared-svcs_vpc_cidr = cidrsubnet(local.super_cidr_block, 4, 1)
-  // The 3rd /20 in 10.10x.0.0/16
-#  spoke_vpc_a_cidr    = cidrsubnet(module.prerequisites.super_cidr_block, 4, 2)
-  // The 4th /20 in 10.10x.0.0/16
-#  spoke_vpc_b_cidr    = cidrsubnet(module.prerequisites.super_cidr_block, 4, 3)
-#  inspection_vpc_cidr    = cidrsubnet(local.super_cidr_block, 8, 253)
-#  ctrl_pln_vpc_cidr = cidrsubnet(local.super_cidr_block, 8, 254)
 
-#  subnet_size_bits     = 3
-#  vpc_name             = "inspection_vpc"
-#  transit_gateway_id   = data.consul_keys.tgw_1.var.id
-
-  cost_tracking_tags   = {
+  cost_tracking_tags = {
     BusinessDepartment = "Technology"
     Domain             = "SecOps"
     EnvironmentType    = "dev"
--- scz-low/us-east-2/prerequisite-root-m/variables.tf
+++
@@ -1,5 +0,0 @@
-#variable "anfw_enabled" {
-#  description = "Set to false to disable creation of the AWS Network Firewall"
-#  type        = bool
-#  default     = true
-#}
