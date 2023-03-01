2ef616af62b748f1521aba3d89c7612508828155
mw-gh-jcastano <john.castano@mavenwave.com>
2023-02-28 16:56:13 -0800

    moved prequiresites into a source module in aws-anfw-module repo

--- scz-low/us-east-2/prerequisites/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "s3-anfw-scz-low-us-east-2"
-    key     = "aws-anfw-module-prerequisite_scz-low_us-east-2.tfstate"
-    region  = "us-east-2"
-    profile = "mw-occ"
-  }
-}
